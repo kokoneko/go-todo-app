7e51743edcac2ec52909b043432550ab49fccaf9
cocowan <maegawaomaru@gmail.com>
2022-02-10 22:59:39 +0900

    環境調整

--- src/main.go
+++ src/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Hello golang from docker!")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.GET("/", hello)
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "hello world")
 }
